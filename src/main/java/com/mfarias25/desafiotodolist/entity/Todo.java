7ae8b1a06241d02a42348011d0741667e241134c
Mateus Farias <mateusfariasdev@outlook.com>
2023-09-26 02:49:25 -0300

    implemented to-do list

--- src/main/java/com/mfarias25/desafiotodolist/entity/Todo.java
+++ src/main/java/com/mfarias25/desafiotodolist/entity/Todo.java
@@ -14,6 +14,14 @@ public class Todo {
     private boolean realizado;
     private int prioridade;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
